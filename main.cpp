47d2fb3cc0e2067710fff9ac9fe995174c204c9c
girvandip <girvandip@gmail.com>
2018-02-09 21:12:22 +0700

    added square.txt file

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main() {
     Point point2(1,1);
     Line line(point1, point2);
     
-    polygon.scanLine(line, 0, 0, 0);
-    polygon.print(0, 0, 255, 255, 255);
+    //polygon.scanLine(line, 0, 0, 0);
+    polygon.rotate(45);
+    polygon.scale(2);
+    polygon.print(200, 200, 255, 255, 255);
 
     return 0;
 }
