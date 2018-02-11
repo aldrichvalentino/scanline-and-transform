72e5d5310d4db412c6d654e1039bee0af955ba70
girvandip <girvandip@gmail.com>
2018-02-12 00:02:28 +0700

    remove bounce from main.cpp

--- main.cpp
+++ main.cpp
@@ -6,30 +6,8 @@
 using namespace std;
 
 
-void bounce(Polygon p,float ratio){
-	float height = 200;
-	while (height * ratio > 0) {
-		for (int i = 0; i < height; ++i){
-			Util::clearScreen();
-
-			p.scanLine(150, 0, 150);
-			p.print(0, 0, 0, 255, 0);
-			p.fall();
-		}
-		height *= ratio;
-		for (int i = 0; i < height; ++i){
-			Util::clearScreen();
-
-			p.scanLine(150, 0, 150);
-			p.print(0, 0, 0, 255, 0);
-			p.up();
-		}
-	}
-}
-
-
 int main() {
-    Polygon plane((char*)"objects/tire.txt");
+    Polygon plane((char*)"objects/plane.txt");
     plane.update(300, 300);
 //    for(int i = 0; i < 200; i++){
 //        Util::clearScreen();
@@ -42,7 +20,7 @@ int main() {
       //  for(int j=0; j < 50000; j++);
     //}
     
-    bounce(plane,0.8);
+    plane.bounce(0.8);
     
     return 0;
 }
